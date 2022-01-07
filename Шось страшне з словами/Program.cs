73ea54867c0be53e04b5f116c8261fd9e4bcc822
gusus21 <yanenko.yurii1121@vu.cdu.edu.ua>
2022-01-07 13:07:02 +0200

    Add a string project

---
+++ Шось страшне з словами/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Шось_страшне_з_словами
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Trim().Split();
+            Console.WriteLine(MadeWord(data));
+        }
+        static string MadeWord(string[] data)
+        {
+            string minSl = data[0];
+            for (int i = 0; i < data.Length; i++)
+            {
+                string k = data[i];
+                int a = k.Length;
+                for (int j = 0; j < k.Length; j++)
+                {
+                    if (k[j] == ',' || k[j] == '!' || k[j] == '.' || k[j] == ':' || k[j] == ';')
+                    {
+                        k.Remove(i, 1);
+                    }
+                }
+                if (k.Length < minSl.Length)
+                {
+                    minSl = k;
+                }
+            }
+            return minSl;
+        }
+    }
+}
